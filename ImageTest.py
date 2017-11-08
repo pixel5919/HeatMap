bba7c386352be937f41eaeb51e2a6453f823cf96
pixel5919 <reboot5919@yahoo.com>
2017-11-07 22:31:25 -0800

    Created first Python File

---
+++ ImageTest.py
@@ -0,0 +1,10 @@
+#!/usr/local/opt/python/libexec/bin/python
+# Yes, I know bad practice but I have 2 pythons
+
+import PIL
+
+def displayHello():
+	print("Hello!\n")
+
+if __name__ == "__main__":
+	displayHello()
